d7f242880453cb3c7a26fd98842bc8c20861ff52
franklynekirwa2 <sangkirwa2@gmail.com>
2024-07-18 12:15:17 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+function addingEventListener(){
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
